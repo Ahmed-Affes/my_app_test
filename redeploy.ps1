95c9c25f2c629fa8bfd46fb6c133921570f03bce
ahmed <ahmedaffes420@gmail.com>
2026-05-14 17:36:12 +0100

    docs: add k8s deployment, service and redeploy script

---
+++ redeploy.ps1
@@ -0,0 +1,14 @@
+# Variables (remplace par ton username Docker Hub)
+$imageName = "<ton-username>/mon-image:latest"
+$deploymentName = "mon-appli"
+
+Write-Host "Construction de l'image Docker..."
+docker build -t $imageName .
+
+Write-Host "Redémarrage du déploiement Kubernetes..."
+kubectl rollout restart deployment $deploymentName
+
+Start-Sleep -Seconds 5
+
+Write-Host "Pods en cours..."
+kubectl get pods
